fed23d5132ae6e295c9a276e604ee2643b10864f
Haider Ali <rmha62@gmail.com>
2024-08-19 18:22:32 +0500

    Adding permissions

---
+++ permissions/get.js
@@ -0,0 +1,35 @@
+function getElementByXpath(path) {
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue
+}
+
+const text = "Only actions bound by a route are listed below."
+
+const elements = Array.from(
+  getElementByXpath("//*[contains(text(),\"" + text + "\")]").parentElement
+    .nextSibling.children
+)
+
+let previous = null
+
+for (const element of elements) {
+  element.children[0].children[0].click()
+  await new Promise((r) => setTimeout(r, 1000))
+  if (!previous) {
+    previous = [element, element.outerHTML]
+    continue
+  }
+  previous[0].outerHTML = previous[1]
+  previous = [element, element.outerHTML]
+}
+
+Object.fromEntries(
+  Array.from(document.querySelectorAll("[type=\"checkbox\"]"))
+    .filter((e) => e.parentElement.nextSibling.innerText !== "Select all")
+    .map((e) => [e.name, e.checked])
+)
