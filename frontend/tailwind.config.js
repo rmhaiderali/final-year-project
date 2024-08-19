d2bbccdbafe85e72393fb6f4082929003bb901bc
Haider Ali <rmha62@gmail.com>
2024-08-19 18:25:38 +0500

    Adding login/signup/home/updateProfile/postJob/viewJobs pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,4 +15,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
