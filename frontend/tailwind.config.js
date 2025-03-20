e040107262b373435a89202630221ac54e77eac7
Haider Ali <rmha62@gmail.com>
2025-03-20 22:09:58 +0500

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/utils/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
